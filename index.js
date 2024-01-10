d18adf884e35a82cc0e521a2f75991258883e6b6
EStherMmutisya <starjacob994@gmail.com>
2024-01-10 23:30:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  // Reverse the cleaned word
+  const reversedWord = cleanedWord.split("").reverse().join("");
+
+  // Check if the cleaned word is the same as its reversed version
+  return cleanedWord === reversedWord;
 }
+
 
 /* 
   Add your pseudocode here
